0f8e7d7d82bfc1f454e798050bcdb48a2293d983
lizzzon <14122002lizard@gmail.com>
2022-12-10 19:00:04 +0300

    [DB] task-5 done

--- credit_scoring_database/triggers.sql
+++ credit_scoring_database/triggers.sql
@@ -1,7 +1,31 @@
-CREATE OR REPLACE FUNCTION user_insert() RETURNS event_trigger AS $Users_Insert$
-BEGIN
-    RAISE NOTICE 'Add user: ', username;
-END;
-$Users_Insert$ LANGUAGE plpgsql;
+-- TRIGGERS
+CREATE OR REPLACE FUNCTION user_insert_logs()
+RETURNS TRIGGER AS $logging$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            INSERT INTO logs SELECT 'Inserting user', NEW.id, now();
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$logging$ LANGUAGE plpgsql;
 
-CREATE EVENT TRIGGER user_insert ON ddl_command_start EXECUTE PROCEDURE user_insert();
+CREATE OR REPLACE TRIGGER insert_logging
+AFTER INSERT ON users
+FOR EACH ROW EXECUTE FUNCTION user_insert_logs();
+
+
+CREATE OR REPLACE FUNCTION user_delete_logs()
+RETURNS TRIGGER AS $logging$
+    BEGIN
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO logs SELECT 'Deleting user', OLD.id, now();
+            RETURN OLD;
+        END IF;
+        RETURN NULL; -- возвращаемое значение для триггера AFTER игнорируется
+    END;
+$logging$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER delete_logging
+BEFORE DELETE ON users
+FOR EACH ROW EXECUTE FUNCTION user_delete_logs();
