e3d0f3d5d01cb5da18a703c914c14f937df46bb3
lizzzon <14122002lizard@gmail.com>
2022-12-12 02:23:01 +0300

    [DB] task-5 fix bugs

--- credit_scoring_database/triggers.sql
+++ credit_scoring_database/triggers.sql
@@ -1,31 +1,25 @@
 -- TRIGGERS
-CREATE OR REPLACE FUNCTION user_insert_logs()
+CREATE OR REPLACE FUNCTION user_logs()
 RETURNS TRIGGER AS $logging$
+DECLARE log_msg varchar;
+DECLARE role_nm varchar;
     BEGIN
-        IF (TG_OP = 'INSERT') THEN
-            INSERT INTO logs SELECT 'Inserting user', NEW.id, now();
-            RETURN NEW;
+        IF (TG_OP = 'DELETE') THEN
+            log_msg := 'Delete user';
+            role_nm := 'moder';
+        ELSIF (TG_OP = 'UPDATE') THEN
+            log_msg := 'Update user';
+            role_nm := 'moder';
+        ELSIF (TG_OP = 'INSERT') THEN
+            log_msg := 'Insert user';
+            role_nm := 'moder';
         END IF;
+        INSERT INTO logs (user_id, staff_role_id, log_type, log_message, time_stamp)
+        VALUES (NEW.id, (SELECT id FROM staff_roles WHERE staff_role_name = role_nm), 'info', log_msg, now());
         RETURN NULL;
     END;
 $logging$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER insert_logging
+CREATE OR REPLACE TRIGGER user_logging_trigger
 AFTER INSERT ON users
-FOR EACH ROW EXECUTE FUNCTION user_insert_logs();
-
-
-CREATE OR REPLACE FUNCTION user_delete_logs()
-RETURNS TRIGGER AS $logging$
-    BEGIN
-        IF (TG_OP = 'DELETE') THEN
-            INSERT INTO logs SELECT 'Deleting user', OLD.id, now();
-            RETURN OLD;
-        END IF;
-        RETURN NULL; -- возвращаемое значение для триггера AFTER игнорируется
-    END;
-$logging$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE TRIGGER delete_logging
-BEFORE DELETE ON users
-FOR EACH ROW EXECUTE FUNCTION user_delete_logs();
+FOR EACH ROW EXECUTE FUNCTION user_logs();
